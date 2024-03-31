c28b34ada1a0cd23f6d15fa4a567208c1388a922
Philip Jurke <pjurke@icloud.com>
2024-03-31 20:36:55 +0200

    add timeline sample page

--- app/components/top-level-menu/index.tsx
+++ app/components/top-level-menu/index.tsx
@@ -4,6 +4,7 @@ const TopLevelMenu = () => {
     return (
         <div className="border-b border-black flex">
             <TopLevelMenuButton title="Home" link="/" />
+            <TopLevelMenuButton title="Timeline" link="/timeline" />
         </div>
     );
 };
---
+++ app/timeline/page.tsx
@@ -0,0 +1,12 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Timeline"
+};
+
+export default function Page() {
+    return (
+      <h1>Timeline Page</h1>
+    );
+  }
+  
