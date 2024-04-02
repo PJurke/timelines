0c12117fbadb63fdc5594eccf6afbf9fcf1b15bf
Philip Jurke <pjurke@icloud.com>
2024-04-02 20:48:05 +0200

    add toggle feature

--- app/components/top-level-menu/HamburgerMenu.tsx
+++ app/components/top-level-menu/HamburgerMenu.tsx
@@ -1,13 +1,18 @@
-const HamburgerMenu = () => {
+const HamburgerMenu = (props: HamburgerMenuProps) => {
+
     return (
-        <div className="md:hidden flex items-center">
-            <button className="mobile-menu-button">
+        <div className="md:hidden flex items-center p-4">
+            <button className="mobile-menu-button" onClick={props.toggleFunction}>
                 <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />
                 </svg>
             </button>
         </div>
     );
 };
+
+interface HamburgerMenuProps {
+    toggleFunction: () => void
+}
 
 export default HamburgerMenu;
--- app/components/top-level-menu/index.tsx
+++ app/components/top-level-menu/index.tsx
@@ -1,13 +1,27 @@
+"use client"
+
+import React, { useState } from 'react';
 import HamburgerMenu from "./HamburgerMenu";
 import TopLevelMenuButton from "./button";
 
 const TopLevelMenu = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleClick = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
 
         <nav className="bg-gray-800 border-b border-black flex text-white">
-            <TopLevelMenuButton title="Home" link="/" />
-            <TopLevelMenuButton title="Timeline" link="/timeline" />
-            <HamburgerMenu />
+            <HamburgerMenu toggleFunction={handleClick} />
+            {isOpen && (
+                <>
+                <TopLevelMenuButton title="Home" link="/" />
+                <TopLevelMenuButton title="Timeline" link="/timeline" />
+                </>
+            )}
         </nav>
 
     );
