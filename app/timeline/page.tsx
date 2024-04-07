cc0a112fb9b93721909fc8617bc6320424384e97
Philip Jurke <pjurke@icloud.com>
2024-04-07 17:01:14 +0200

    making timeline page dynamic

--- app/timeline/page.tsx
+++ app/timeline/page.tsx
@@ -1,21 +1,31 @@
 import type { Metadata } from "next";
 import TimelineEvent from "../components/timeline/event";
+import listTimelines from "../lib/api/get-timeline";
+import { PageProps } from "@/.next/types/app/layout";
 
 export const metadata: Metadata = {
   title: "Timeline"
 };
 
-export default function Page() {
-    return (
-      
-        <div className="p-4">
-          <h1 className="text-4xl">Timeline</h1>
-
-          <TimelineEvent date="2024" title="3rd Event" />
-          <TimelineEvent date="2023" title="2nd Event" />
-          <TimelineEvent date="2022" title="1st Event" />
-        </div>
-
-    );
-  }
+export default async function Page(props: PageProps) {
   
+  if (props.searchParams?.id === undefined) {
+    console.log('nein');
+    return;
+  }
+
+  //const timeline = await listTimelines('6611af95ecf8c97fc6d9ce41');
+  const timeline = await listTimelines(props.searchParams.id);
+  
+  return (
+    
+      <div className="p-4">
+        <h1 className="text-4xl">{timeline?.name}</h1>
+
+        {timeline?.events.map((event) => (
+          <TimelineEvent date={event.date} title={event.title} />
+        ))}
+      </div>
+
+  );
+}
