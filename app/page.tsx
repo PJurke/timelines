800df822af7947764c6f49487fe3d64bedffbbbe
Philip Jurke <pjurke@icloud.com>
2024-04-13 21:54:40 +0200

    add homepage section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
 export default function Home() {
   return (
 
-    <div className="p-4">
-      <h1>Hello World</h1>
-    </div>
+    <section className="h-screen flex flex-col">
+      <div className="flex-grow flex items-center justify-center flex-col px-4 text-center">
+        <h1 className="text-5xl font-bold text-gray-800 mb-4">
+          Timelines
+        </h1>
+        <p className="text-gray-600 text-lg mb-8">
+          Timelines are highly versatile visualization tools that can be used to represent a wide array of information.<br/>
+        </p>
+      </div>
+    </section>
 
   );
 }
