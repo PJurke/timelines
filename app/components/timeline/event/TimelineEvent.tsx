394d1840405f38ba79c890fbbd9928b633d651a6
Philip Jurke <pjurke@icloud.com>
2024-07-07 13:34:15 +0200

    change timeline event color

--- app/components/timeline/event/TimelineEvent.tsx
+++ app/components/timeline/event/TimelineEvent.tsx
@@ -4,7 +4,7 @@ const TimelineEvent = (props: TimelineEventProps) => {
     return (
 
         <div className="grid gap-1 text-sm relative">
-            <div className="aspect-square w-3 bg-slate-400 rounded-full absolute left-0 translate-x-[-29.5px] z-10 top-1" />
+            <div className="aspect-square w-3 bg-amber-400 rounded-full absolute left-0 translate-x-[-29.5px] z-10 top-1" />
             <div className="font-medium">{props.date}</div>
             <div>{props.title}</div>
         </div>
