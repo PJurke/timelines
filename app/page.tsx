bc18f182c09a0457a8b8e9ac66b02cef27a2923d
Philip Jurke <pjurke@icloud.com>
2024-07-26 19:53:44 +0200

    adapt intro text

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 export default function Home() {
   return (
 
-    <section className="h-screen flex flex-col">
+    <section className="flex flex-col h-screen">
       <div className="flex-grow flex items-center justify-center flex-col px-4 text-center">
         <h1 className="text-5xl font-bold text-gray-800 mb-4">
           Timelines
         </h1>
         <p className="text-gray-600 text-lg mb-8">
-          Timelines are highly versatile visualization tools that can be used to represent a wide array of information.<br/>
+          Timelines are highly versatile visualization tools that represent a sequence of events and milestones, offering clarity on how things evolve over time.
         </p>
       </div>
     </section>
