91f2fc7bcb19855b85deff23a7027d7532866982
Philip Jurke <pjurke@icloud.com>
2024-08-14 20:12:06 +0200

    timeline event circle thicker

--- app/components/timeline/event/TimelineEventItem.tsx
+++ app/components/timeline/event/TimelineEventItem.tsx
@@ -4,7 +4,7 @@ import { TimelineEventProps } from "./TimelineEventProps";
 export default function TimelineEventItem(props: TimelineEventProps) {
     return (
         <div className="flex items-center md:space-x-6">
-            <div className="bg-white border border-black hidden h-6 rounded-full w-6 md:block"></div>
+            <div className="bg-white border-2 border-black hidden h-6 rounded-full w-6 md:block"></div>
             <TimelineEventDetails key={props.date + props.title} date={props.date} title={props.title} />
         </div>
     );
