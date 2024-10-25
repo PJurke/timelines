5abffc92b850e8a0f790e17910c3c8506fcee013
Philip Jurke <pjurke@icloud.com>
2024-10-25 13:36:23 +0200

    add accessibility

--- app/components/top-level-menu/HamburgerMenu.tsx
+++ app/components/top-level-menu/HamburgerMenu.tsx
@@ -16,7 +16,8 @@ const HamburgerMenu = ({toggleVisibility, isOpen}: HamburgerMenuProps) => {
 
         <div className="p-4 self-end md:hidden">
             <button aria-label={isOpen ? "Close Menu" : "Open Menu"} className="mobile-menu-button" onClick={toggleVisibility} type="button">
-                <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <title>{isOpen ? "Close Menu" : "Open Menu"}</title>
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d={isOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16m-7 6h7"} />
                 </svg>
             </button>
